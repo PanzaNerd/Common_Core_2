6f0b857778447b43f0747f7dc3a11ea749a93c3c
mpanzani <matthiaspanzani@gmail.com>
2026-05-20 19:39:55 +0200

    mercoledi esame

---
+++ exam02/level0/fizzbuzz.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fizzbuzz.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpanzani <mpanzani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/20 18:25:28 by mpanzani          #+#    #+#             */
+/*   Updated: 2026/05/20 19:11:36 by mpanzani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putnbr(int i)
+{
+    char c;
+
+    if (i >= 10)
+        ft_putnbr(i / 10);
+    c = (i % 10) + '0';
+    write(1, &c, 1);
+}
+
+int	main(int ac, char **av)
+{
+	int i = 1;
+
+	if(ac == 1)
+	{
+		while(i <= 100)
+		{
+			if(i % 3 == 0 && i % 5 == 0)
+				write(1, "fizzbuzz", 8);
+			else if(i % 3 == 0)
+				write(1, "fizz", 4);
+			else if(i % 5 == 0)
+				write(1, "buzz", 4);
+			else
+				ft_putnbr(i);
+			write(1, "\n", 1);
+			i++;
+		}
+	}
+	return(0);
+}
---
+++ exam02/level0/ft_putstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpanzani <mpanzani@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/20 19:32:28 by mpanzani          #+#    #+#             */
+/*   Updated: 2026/05/20 19:39:01 by mpanzani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int i = 0;
+	
+	while(str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	char str[]= "krankenwagen";
+	
+	ft_putstr(str);
+	return(0);
+}
