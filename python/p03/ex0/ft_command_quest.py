2d42e37fe4bdbfe80263c2084157560ce6e8943a
mpanzani <matthiaspanzani@gmail.com>
2026-09-10 17:52:54 +0200

    uwgviuwg

--- python/p03/ex0/ft_command_quest.py
+++ python/p03/ex0/ft_command_quest.py
@@ -5,8 +5,26 @@
 #                                                     +:+ +:+         +:+      #
 #    By: matthias <matthias@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/09/02 18:34:45 by matthias          #+#    #+#              #
-#    Updated: 2026/09/02 18:34:46 by matthias         ###   ########.fr        #
+#    Created: 2026/09/10 10:00:00 by matthias          #+#    #+#              #
+#    Updated: 2026/09/10 10:00:00 by matthias         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+import sys
+
+def main() -> None:
+	print("=== Command Quest ===")
+	print(f"Program name: {sys.argv[0]}")
+
+	if len(sys.argv) == 1:
+		print("No arguments provided!")
+	else:
+		print(f"Arguments received: {len(sys.argv) - 1}")
+		for i in range(1, len(sys.argv)):
+			print(f"Argument {i}: {sys.argv[i]}")
+
+	print(f"Total arguments: {len(sys.argv)}")
+
+
+if __name__ == "__main__":
+	main()
